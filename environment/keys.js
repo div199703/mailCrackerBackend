e93ef8d4906cc47b02284cd6bc5044b6befc4530
divyanjana1997@gmail.com <divyanjana1997@gmail.com>
2021-01-09 21:52:10 +0530

    test change after adding to gitignore

--- environment/keys.js
+++ environment/keys.js
@@ -1,8 +1,8 @@
-const prodKeys = require("./keys.prod");
-process.env.NODE_ENV = "production";
+// const prodKeys = require("./keys.prod");
+// process.env.NODE_ENV = "production";
 
-// const devKeys = require("./keys.dev");
-// process.env.NODE_ENV = "development";
+const devKeys = require("./keys.dev");
+process.env.NODE_ENV = "development";
 
 if (process.env.NODE_ENV === "production") {
     module.exports = prodKeys;
