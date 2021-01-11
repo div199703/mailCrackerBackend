5ff50f193e3cde1852890705dd0fd4a82cdef691
divyanjana1997@gmail.com <divyanjana1997@gmail.com>
2021-01-11 09:32:38 +0530

    added uid

--- environment/keys.js
+++ environment/keys.js
@@ -1,13 +1,11 @@
-const prodKeys = require("./keys.prod");
-process.env.NODE_ENV = "production";
+// const prodKeys = require("./keys.prod");
+// process.env.NODE_ENV = "production";
 
-// const devKeys = require("./keys.dev");
-// process.env.NODE_ENV = "development";
+const devKeys = require("./keys.dev");
+process.env.NODE_ENV = "development";
 
 if (process.env.NODE_ENV === "production") {
     module.exports = prodKeys;
 } else {
     module.exports = devKeys;
 }
-
-// test
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const nodemailer = require('nodemailer');
 const keys = require('./environment/keys');
 var cors = require('cors');
+var getUid = require('get-uid');
 
 const Server = keys.OWN_SERVER;
 const app = express();
@@ -14,72 +15,89 @@ app.use(bodyParser.json())
 app.use(cors())
 
 var conn = mysql.createConnection({
-    host: keys.DB_HOST,
-    user: keys.DB_USER,
-    password: keys.DB_PASSWORD,
-    database: keys.DB_DATABASE
+  host: keys.DB_HOST,
+  user: keys.DB_USER,
+  password: keys.DB_PASSWORD,
+  database: keys.DB_DATABASE
 });
 
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: keys.CONFIG_EMAIL,
-      pass: keys.CONFIG_EMAIL_PASS
-    }
+  service: 'gmail',
+  auth: {
+    user: keys.CONFIG_EMAIL,
+    pass: keys.CONFIG_EMAIL_PASS
+  }
 });
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is listening...url: "+Server);
+  console.log("Server is listening...url: " + Server);
 });
 
 app.get('/', (req, res) => {
-    res.json({
-      "message": "Hi this is mail tracker node server",
-      "url": Server
+  res.json({
+    "message": "Hi this is mail tracker node server",
+    "url": Server
   });
 });
 
-app.route('/recipients/:recipient').get((req, res) => {
+app.route('/recipients/:recipient/:uid').get((req, res) => {
   var Recipient = req.params['recipient'];
+  var uid = req.params['uid'];
   var date_ob = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  conn.query('UPDATE recipients SET opened = true, lastseen= ? WHERE email=?', [date_ob,Recipient],
-  (err, rows) => {
-    if (err) {
-      console.log(err);
-    }else{
-      console.log('Data Inserted:');
-      res.send('data inserted')
-    }
-  });
+  conn.query('UPDATE recipients SET opened = true, lastseen= ? WHERE email=? AND uid=?', [date_ob, Recipient, uid],
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Data Inserted:');
+        res.send({ "status": "success" });
+      }
+    });
+  res.send ({"time" : date_ob});
 })
 
 app.route('/sendmail').post((req, res) => {
-    let Sender = req.body['Sender'];
-    let Recipient = req.body['Recipient'];
-    let MessageBody = req.body['MessageBody'];
-    let Subject = req.body['Subject'];
+  let Sender = req.body['Sender'];
+  let Recipient = req.body['Recipient'];
+  let MessageBody = req.body['MessageBody'];
+  let Subject = req.body['Subject'];
+  var uid = getUid();
 
-    let htmlBody = '<p>'+MessageBody+'</p>'+'<img src = "'+Server+'/recipients/'+Recipient+'" style="display:none">';
+  let htmlBody = '<p>' + MessageBody + '</p>' + '<img src = "' + Server + '/recipients/' + Recipient + '/' + uid + '" style="display:none">';
 
-    var mailOptions = {
-        from: Sender,
-        to: Recipient,
-        subject: Subject,
-        html: htmlBody
-    };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          conn.query('INSERT INTO recipients(email) VALUES(?)', [Recipient],
+  var mailOptions = {
+    from: Sender,
+    to: Recipient,
+    subject: Subject,
+    html: htmlBody
+  };
+  if (process.env.NODE_ENV === "production") {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        conn.query('INSERT INTO recipients(email,message,uid) VALUES(?,?,?)', [Recipient,MessageBody,uid],
           (err, rows) => {
             if (err) {
               throw err,
               console.log(err);
-            }else{
-              res.send({"message": "success"});
+            } else {
+              res.send({ "status": "success" });
             }
           });
+      }
+    });
+  }
+  else {
+    //res.send({ "status": "success(local_db)" , "message" : htmlBody});
+    conn.query('INSERT INTO recipients(email,message,uid) VALUES(?,?,?)', [Recipient,MessageBody,uid],
+      (err, rows) => {
+        if (err) {
+          throw err,
+          console.log(err);
+        } else {
+          res.send({ "status": "success(local_db)" , "message" : htmlBody});
         }
       });
-  })
+  }
+})
